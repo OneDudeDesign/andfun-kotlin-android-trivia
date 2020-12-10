3e9f9ee10350a13db8bdb3e3113833219ee5dd81
Christopher Lucier <OneDudeDesign@gmail.com>
2020-12-10 09:40:54 -0800

    Finished Excercise 4

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -38,12 +38,14 @@ import android.content.pm.PackageManager
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
-        // TODO (06) Add an onClick Handler for the nextMatch button that navigates to the gameFragment
-        // using action_gameWonFragment_to_gameFragment
+        binding.nextMatchButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
+
         return binding.root
     }
 }
