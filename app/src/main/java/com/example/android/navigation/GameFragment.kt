0312d9a1efaa6c8985cd456fca9c8d274bc72f1f
Christopher Lucier <OneDudeDesign@gmail.com>
2020-12-10 09:13:22 -0800

    Finished Excercise 3

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -96,17 +96,13 @@ class GameFragment : Fragment() {
                         currentQuestion = questions[questionIndex]
                         setQuestion()
                         binding.invalidateAll()
-                    } else {
-                        // We've won!  Navigate to the gameWonFragment.
-                        // TODO (05) Find the navController from the view and navigate to the gameWonFragment
-                        // call view.findNavController
-                        // navigate to R.id.action_gameFragment_to_gameWonFragment
+                    } else { view.findNavController()
+                            .navigate(R.id.action_gameFragment_to_gameWonFragment)
+
                     }
-                } else {
-                    // Game over! A wrong answer sends us to the gameOverFragment.
-                    // TODO (06) Find the navController from the view and navigate to the gameOverFragment
-                    // call view.findNavController
-                    // navigate to R.id.action_gameFragment_to_gameOverFragment
+                } else { view.findNavController()
+                        .navigate(R.id.action_gameFragment_to_gameOverFragment)
+
                 }
             }
         }
