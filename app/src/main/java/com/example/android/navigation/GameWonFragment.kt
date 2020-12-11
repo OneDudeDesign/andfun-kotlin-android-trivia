6436372713da441112d1788d1ab9ad2db7cf79ad
Christopher Lucier <OneDudeDesign@gmail.com>
2020-12-11 09:04:42 -0800

    Finished Excercise 8

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,21 +46,48 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(
                     GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        var args = GameWonFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context,
-                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
-                Toast.LENGTH_LONG).show()
-        // TODO (01) Add setHasOptionsMenu(true)
+        // (01) Add setHasOptionsMenu(true)
         // This allows onCreateOptionsMenu to be called
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
 
-    // TODO (02) Create getShareIntent method
-    // TODO (03) Create shareSuccess method
-    // TODO (04) Override and fill out onCreateOptionsMenu
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    private fun getShareIntent() : Intent {
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT,
+                        getString(R.string.share_success_text, args.numCorrect,
+                                args.numQuestions))
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    // (02) Create getShareIntent method
+    // (03) Create shareSuccess method
+    // (04) Override and fill out onCreateOptionsMenu
     // Inflate the winner_menu and set the share menu item to invisible if the activity doesn't
     // resolve
-    // TODO (05) Override onOptionsItemSelected
+       // (05) Override onOptionsItemSelected
     // Call the shareSuccess method when the item id matches R.id.share
 }
